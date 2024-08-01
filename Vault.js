0fa6812437676991fb2dd12552cd4148b60a8a7b
Milani <milaniwhite12@gmail.com>
2024-07-31 22:01:33 -0400

    changes

---
+++ Vault.js
@@ -0,0 +1,12 @@
+let message="You have received this message because you have been chosen to open an important vault"
+
+
+let num1=5+5 
+let num2=20*2
+let num3=40-1
+
+
+
+
+
+alert(`${message} ${num1}${num2}${num3}`)
